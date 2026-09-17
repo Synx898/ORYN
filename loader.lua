8bfdda2a53b3b441822388b09d2324f044119f87
SL1YYY <jacobtheman62@gmail.com>
2026-09-17 17:41:44 -0400

    Update loader URL in loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://lua.services/api/projects/bfd364cb35222df0c9c9821aacc3edb5/loader"))()
+loadstring(game:HttpGet("https://lua.services/api/scripts/b8b88d2eb497512bc6d431440ff168e3/loader"))()
